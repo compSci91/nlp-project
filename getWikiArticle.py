db835fd57d65a8e834cd9b05c29ef2ce70d385cb
JoshuaHowell <howjosh@gmail.com>
2018-11-03 12:27:35 -0500

    JH -- Creation of Corpus -- Add files to create corpus automatically as well as 64 Farming related subjects

--- getWikiArticle.py
+++ getWikiArticle.py
@@ -1,14 +1,19 @@
+import sys
 from wikipedia import Wikipedia
 from wiki2plain import Wiki2Plain
 
-lang = 'simple'
+lang = 'en'
 wiki = Wikipedia(lang)
 
 try:
-    raw = wiki.article('Uruguay')
+    articleName = str(sys.argv[1])#'Uruguay'
+    raw = wiki.article(articleName)
 except:
     raw = None
+
 if raw:
     wiki2plain = Wiki2Plain(raw)
+    f = open('./corpus/' + articleName + '.txt', 'w')
+    f.write(wiki2plain.text)
     #content = wiki2plain.text
-    print(wiki2plain.text)
+    #print(wiki2plain.text)
