c8ad387967101e89a39358db34fecf541d349de5
JoshuaHowell <howjosh@gmail.com>
2018-11-06 13:02:59 -0600

    JH -- NGrams Refactoring -- Extract calculation of the number of bigrams in common.

--- main.py
+++ main.py
@@ -5,7 +5,20 @@
 #
 # file.close
 
+
+# def printSomeStuff():
+#     print "Hello, methods!"
+#
+# printSomeStuff()
+
 from nltk import ngrams
+
+def calculateNumberOfBigramsInCommon(firstBigrams, secondBigrams):
+    number_of_bigrams_in_common = 0
+    for bigram in firstBigrams:
+        if bigram in secondBigrams:
+            number_of_bigrams_in_common += 1
+    return number_of_bigrams_in_common
 
 anna_karenina_constance_garnett = "Happy families are all alike; every unhappy family is unhappy in its own way.".lower()
 
@@ -31,13 +44,4 @@ bigrams = ngrams(anna_karenina_schwartz.split(), 2)
 for bigram in bigrams:
     anna_karenina_schwartz_bigrams.add(bigram)
 
-#print anna_karenina_schwartz_bigrams
-
-number_of_bigrams_in_common = 0
-
-for anna_karenina_constance_garnett_bigram in anna_karenina_constance_garnett_bigrams:
-    if anna_karenina_constance_garnett_bigram in anna_karenina_schwartz_bigrams:
-        number_of_bigrams_in_common += 1
-        print anna_karenina_constance_garnett_bigram
-
-print "Number of bigrams in common ", number_of_bigrams_in_common
+print calculateNumberOfBigramsInCommon(anna_karenina_schwartz_bigrams, anna_karenina_constance_garnett_bigrams)
