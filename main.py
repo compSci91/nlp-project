e596890e2853073b40fbc9358f1498458907024c
JoshuaHowell <howjosh@gmail.com>
2018-11-06 13:25:32 -0600

    JH -- NGrams -- Assorted refactorings and experiments, none of which proved fruitful. The code still works, though.

--- main.py
+++ main.py
@@ -14,10 +14,13 @@
 from nltk import ngrams
 
 def calculateNumberOfBigramsInCommon(firstBigrams, secondBigrams):
+
     number_of_bigrams_in_common = 0
     for bigram in firstBigrams:
         if bigram in secondBigrams:
+            print bigram
             number_of_bigrams_in_common += 1
+
     return number_of_bigrams_in_common
 
 anna_karenina_constance_garnett = "Happy families are all alike; every unhappy family is unhappy in its own way.".lower()
@@ -29,7 +32,7 @@ bigrams = ngrams(anna_karenina_constance_garnett.split(), 2)
 for bigram in bigrams:
     anna_karenina_constance_garnett_bigrams.add(bigram)
 
-#print anna_karenina_constance_garnett_bigrams
+# print anna_karenina_constance_garnett_bigrams
 
 
 
@@ -44,4 +47,6 @@ bigrams = ngrams(anna_karenina_schwartz.split(), 2)
 for bigram in bigrams:
     anna_karenina_schwartz_bigrams.add(bigram)
 
-print calculateNumberOfBigramsInCommon(anna_karenina_schwartz_bigrams, anna_karenina_constance_garnett_bigrams)
+
+ print calculateNumberOfBigramsInCommon(anna_karenina_schwartz_bigrams, anna_karenina_constance_garnett_bigrams)
+#print calculateNumberOfBigramsInCommon(anna_karenina_constance_garnett_bigrams, anna_karenina_schwartz_bigrams)
